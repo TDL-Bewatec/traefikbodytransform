900f37cd48489f6f4bd5161afebd9cd506ef6dcc
Daniel Losinzky <97226936+TDL-Bewatec@users.noreply.github.com>
2024-04-25 09:39:00 +0200

    cleanup

--- transformer.go
+++ transformer.go
@@ -13,16 +13,13 @@ import (
 
 // Config the plugin configuration.
 type Config struct {
-	TransformerQueryParameterName         string `json:"transformerQueryParameterName,omitempty"`
-	JSONTransformFieldName                string `json:"jsonTransformFieldName,omitempty"`
+
 	TokenTransformQueryParameterFieldName string `json:"tokenTransformQueryParameterFieldName,omitempty"`
 }
 
 // CreateConfig creates the default plugin configuration.
 func CreateConfig() *Config {
 	return &Config{
-		TransformerQueryParameterName:         "transformer",
-		JSONTransformFieldName:                "data",
 		TokenTransformQueryParameterFieldName: "token",
 	}
 }
@@ -31,8 +28,6 @@ func CreateConfig() *Config {
 type transformer struct {
 	next                                  http.Handler
 	name                                  string
-	transformerQueryParameterName         string
-	jsonTransformFieldName                string
 	tokenTransformQueryParameterFieldName string
 }
 
@@ -42,8 +37,6 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 	return &transformer{
 		next:                                  next,
 		name:                                  name,
-		transformerQueryParameterName:         config.TransformerQueryParameterName,
-		jsonTransformFieldName:                config.JSONTransformFieldName,
 		tokenTransformQueryParameterFieldName: config.TokenTransformQueryParameterFieldName,
 	}, nil
 }
@@ -56,7 +49,6 @@ func (a *transformer) log(format string) {
 }
 
 func (a *transformer) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	fmt.Println("traefikbodytransform ==> inside ServeHTTP")
 	transformerOption := make(map[string]bool)
     token := req.URL.Query().Get(a.tokenTransformQueryParameterFieldName)
 	fmt.Println("traefikbodytransform:token ==>", token)
